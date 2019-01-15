694d1d3796f5e40c44bd63ee38943ae45af4fab8
“RickSunder” <rick@sunder.nl>
2019-01-15 11:59:40 +0000

    hopefully layout works

--- controller/helpers.py
+++ controller/helpers.py
@@ -13,6 +13,6 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            return redirect("/login")
+            return redirect("/homepage")
         return f(*args, **kwargs)
     return decorated_function
