fb8606179d91e8f3833aec3bb72488a23f0a45bc
“RickSunder” <rick@sunder.nl>
2019-01-15 12:36:13 +0000

    test

---
+++ application.py
@@ -0,0 +1,44 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+import unicodedata
+
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///PicUs.db")
+
+@app.route("/")
+def homepage():
+    """The homepage of the website"""
+    if request.method == "POST":
+        return "hoi"
+    else:
+        return render_template("homepage.html")
+def make_group():
+    """Make new group"""
+    if request.method == "POST":
+        return "bla"
+    else:
+        return "joe"
+
